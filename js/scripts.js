06940c754374da4fc5842f16de2faafd58db422c
meglybbert <mlybbert1@vcd-ceb131-18.eastern.ewu.edu>
2017-04-13 16:01:40 -0700

    Pulled in simpleWeather

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
 // Scripts
-console.log("Hello World!");
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city); 
+      $('.image img').attr('src', weather.image);
+      $('.description p').text(weather.text);
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+//Activate accordion
+$('.ui.accordion')
+  .accordion()
+;
